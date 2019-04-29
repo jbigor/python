cb24dadfdab1b6a60cb86cfdee1f360a48f37cb2
Igor Shbanov <admin@naocompany.ru>
2019-04-29 11:35:19 +0300

    domashnya rabota

---
+++ dz.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+    
+for file in *txt
+
+do
+ 	grep -v "^[[:space:]]" $file | tr -d "\r" >tempfile
+	sed ’/^\s*$/d’ tempfile
+	sed -r 's/[A-Z]*[A-Z]/abbreviation/g' $file >tempfile	
+mv tempfile $file
+
+done 
