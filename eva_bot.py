9234f5c9fadc2cd2fade497e09008bfb4b971380
Igor Shbanov <50086576+jbigor@users.noreply.github.com>
2019-05-08 16:33:22 +0300

    Update eva_bot.py

--- eva_bot.py
+++ eva_bot.py
@@ -1,13 +1,46 @@
 # начало разработки искуственного ителекта в linux
 # думаю в будущем она будет говорить и заражать все компьютеры
-
 import speech_recognition as sr
+import pyttsx3
 import os
 import sys
 import webbrowser
+import pyaudio
 
-def talk(words):
-    print(words)
-    os.system("say" + words)
+def output(x):
+    engine = pyttsx3.init()
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate-70)
+    print("Pc: "+x+".")
+    engine.say(x)
+    engine.runAndWait()
+output("Hello, how are you")
 
-talk("Привет, я ева")
+def command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("say")
+        r.pause_threshold = 1
+        r.listen(source)
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio).lower()
+        print("Вы сказали:" + command)
+    except sr.UnknownValueError:
+       output("Error")
+        zadanie = command()
+
+    return zadanie
+def makeSomething(zadanie):
+    if 'open website' in zadanie:
+        output("Open Web site")
+        url = 'http://ya.ru'
+        webbrowser.open(url)
+    elif 'stop' in zadanie:
+
+        sys.exit()
+    while True:
+        makeSomething(command())
+
+
