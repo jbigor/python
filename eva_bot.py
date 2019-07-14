043f570c3809400458dad9333fe81e349ed2211d
Igor Shbanov <50086576+jbigor@users.noreply.github.com>
2019-07-14 16:17:14 +0300

    Update eva_bot.py

--- eva_bot.py
+++ eva_bot.py
@@ -1,46 +1,61 @@
-# начало разработки искуственного ителекта в linux
-
 import speech_recognition as sr
-import pyttsx3
 import os
 import sys
 import webbrowser
-import pyaudio
 
-def output(x):
-    engine = pyttsx3.init()
-    rate = engine.getProperty('rate')
-    engine.setProperty('rate', rate-70)
-    print("Pc: "+x+".")
-    engine.say(x)
-    engine.runAndWait()
-output("Hello, how are you")
+
+def talk(words):
+	print(words)
+	os.system("say " + words)
+
+
+talk("Привет,,,")
 
 def command():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("say")
-        r.pause_threshold = 1
-        r.listen(source)
-        r.adjust_for_ambient_noise(source, duration=1)
-        audio = r.listen(source)
-    try:
-        command = r.recognize_google(audio).lower()
-        print("Вы сказали:" + command)
-    except sr.UnknownValueError:
-       output("Error")
-        zadanie = command()
+	r = sr.Recognizer()
 
-    return zadanie
+	with sr.Microphone() as source:
+		print("Говорите")
+		r.pause_threshold = 1
+		r.adjust_for_ambient_noise(source, duration=1)
+		audio = r.listen(source)
+
+	try:
+		zadanie = r.recognize_google(audio, language="ru-RU").lower()
+		print("Вы сказали: " + zadanie)
+	except sr.UnknownValueError:
+		talk(" ")
+		zadanie = command()
+
+	return zadanie
+
 def makeSomething(zadanie):
-    if 'open website' in zadanie:
-        output("Open Web site")
-        url = 'http://ya.ru'
-        webbrowser.open(url)
-    elif 'stop' in zadanie:
 
-        sys.exit()
-    while True:
-        makeSomething(command())
+	if 'открой сайт' in zadanie:
+		talk("Уже открываю")
+		url = 'http://ya.ru'
+		webbrowser.open(url)
+	elif 'ева открой сайт' in zadanie:
+		talk("Подожди открываю")
+		url = 'http://ya.ru'
+		webbrowser.open(url)
+	elif 'привет' in zadanie:
+		talk("Привет, Игорь")
+		sys.exc_info()
+
+	elif 'стоп' in zadanie:
+		talk("Пока, Игорь")
+		sys.exit()
+	elif 'как тебя зовут' in zadanie:
+		talk("Игорь, меня зовут ева")
+		sys.exc_info()
+	elif 'сколько тебе лет' in zadanie:
+		talk("я не знаю")
+		sys.exc_info()
 
 
+
+
+
+while True:
+	makeSomething(command())
